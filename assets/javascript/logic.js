439bc36108a620376e03ffc4923d2480b18225b1
Mark Pahor <mark@Marks-MBP.socal.rr.com>
2019-06-08 08:42:30 -0700

    let's hope I don't lose all this

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -12,41 +12,15 @@
 //     method: "GET"
 //     })
 //     .then(function(response) {
-
-//         // Log the queryURL
 //         console.log(response);
-
 //     });
 
 
 
-
+//yelp api ajax pull
 var yelpAPIKey = "NNn_iZkgwcsoXyb1LwNcwgRAiCL8c3RkazAkRcQueV0e5b0lZNV-SGGIeosL3AiABzN0_PsQasfbyA8BkbNTjHr-RiTH3sKFAPyB8SCmQInth1SBzlW1uhiuBsr5XHYx"
 
-//var yelpQueryURL = `https://api.yelp.com/v3/businesses/search?term=delis&latitude=37.786882&longitude=-122.399972`;
-
-// $.ajax({
-//     url: yelpQueryURL,
-//     headers: {
-//         "Authorization": `Bearer ${yelpAPIKey}`,
-//         "cache-control": "no-cache",
-//         "Access-Control-Allow-Origin": "*",
-//     },
-//     dataType: 'jsonp',
-//     method: "GET",
-//     error: function (error) {
-//         console.log(error);
-
-//     }
-// })
-//     .then(function (response) {
-
-//         // Log the queryURL
-//         console.log(response);
-
-//     });
-
-var myurl = "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=restaurants&location=boston";
+var myurl = "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=hotels&location=boston&limit=10";
 
 $.ajax({
     url: myurl,
@@ -61,10 +35,63 @@ $.ajax({
 }).then(function (response) {
     console.log(response);
 
+
+//just messing around to see if I can succesfully pull the response data into the html successfully
+//will have to dynamically create rows with about 10 search results to display the data we want
+//each row will have to be a clickable element to take you to that hote's site, or that restaurant on google maps....
+//var newDiv = $("<div>");
+//var responseImg = $("<img>");
+
+//responseImg.attr({
+    //"src": response.businesses[0].image_url,
+    //"alias": response.businesses[0].alias,
+
+//})
+
+//newDiv.append(responseImg);
+
+//$(".collapsible").append(newDiv)
 });
 
-
-
 //link to firebase
+var config = { 
+    apiKey: "AIzaSyAEHiL6iIGeMKpa6X0dKc1F8fv0qXlgks0",
+    authDomain: "fir-click-counter-7cdb9.firebaseapp.com",
+    databaseURL: "https://travelrush-b1c4f.firebaseio.com/",
+    storageBucket: "gs://travelrush-b1c4f.appspot.com"
+  };
+  
+firebase.initializeApp(config);
+
+var database = firebase.database();
+
+//on click event to capture and store values
+$("#user-input").on("click", function(event) {
+    event.preventDefault();
+    console.log("on click event running")
+
+     //store values from form input
+     var origin = $("#origin-airport").val().trim();      
+     var destination = $("#dest-airport").val().trim();      
+     var departure = moment($("#depart-date").val().trim()).format("L");
+     var arrival = moment($("#return-date").val().trim(),).format("L");    
+     console.log(origin, destination, departure, arrival); 
+     
+   // Create local "temporary" object for holding input data
+   var searchInput = {
+       origin: origin,
+       destination: destination,
+       departDate: departure,
+       returnDate: arrival,
+   };
+ console.log(searchInput)
+
+ //upload object to the database
+   database.ref().push(searchInput);
+})
+
+//create on child added function to take snapshot of database objects and display to search History
+//Take those search parameters and allow the user to click to add them back to the input fields
+//on click of entire row (set row class) display child snapshots in the input fields
 
 //start coding the logic to pull input data and return api results 
